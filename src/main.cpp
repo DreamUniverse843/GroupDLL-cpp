23a59df8ee8f28dc1a92903458d8ee4080c9bcd6
晓宇致梦Universe <53889721+DreamUniverse843@users.noreply.github.com>
2022-11-01 00:31:14 +0800

    Rebuild File I/O for *.ini files
    Using SimpleINI to process ini file.
    Fixed invalid function argument parsing.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include "windows.h"
 #include "time.h"
+#include "simpleINI.h"
 using namespace MiraiCP;
 
 const PluginConfig CPPPlugin::config{
@@ -22,9 +23,53 @@ int isKickIgnoreEnable = 0;
 const long long int Admins[10]={3212903564,344731666,1254207361,767607252,437333682,3476768863,2378612355,3100298105,2720844086};
 //是否取消群成员清理操作(标记使用)
 int isCleanCancelled = 0;
-//群对象，从上至下依次为包子铺、通知群、联合会群。
+CSimpleIniA RobotINI;//ini 文件操作对象
+const std::string iniPath = "C:\\Users\\Server\\Desktop\\Mirai.ini";
 
+void iniInitialize(std::string FilePath) //初始化配置文件存储
+{
+    RobotINI.SetUnicode(); // 默认使用 UTF-8 编码
+    RobotINI.LoadFile(FilePath.c_str());
+    RobotINI.SetValue("Initialize","True","1");
+    RobotINI.SaveFile(FilePath.c_str());
+    Logger::logger.info("[System]启动阶段读取 ini 文件请求成功。");
+}
 
+std::string iniQuery(std::string FilePath,std::string Section,std::string Name) //查询配置文件内的键值
+{
+    RobotINI.SetUnicode();
+    RobotINI.LoadFile(FilePath.c_str());
+    const char * ReturnValue = RobotINI.GetValue(Section.c_str(),Name.c_str(),"未找到");
+    Logger::logger.info("[INISystem]查询字段请求发送成功。");
+    int ReturnCode = RobotINI.SaveFile(FilePath.c_str());
+    if(ReturnCode < 0)
+    {
+        Logger::logger.info("[INISystem]文件写入出现问题。");
+        return "未找到";
+    }
+    else
+    {
+        Logger::logger.info("[INISystem]文件写入成功。");
+        return ReturnValue;
+    }
+}
+
+void iniWrite(std::string FilePath,std::string Section,std::string Name,std::string Value)
+{
+    RobotINI.SetUnicode();
+    RobotINI.LoadFile(FilePath.c_str());
+    RobotINI.SetValue(Section.c_str(),Name.c_str(),Value.c_str());
+    Logger::logger.info("[INISystem]变更字段请求发送成功。");
+    int ReturnCode = RobotINI.SaveFile(FilePath.c_str());
+    if(ReturnCode < 0)
+    {
+        Logger::logger.info("[INISystem]文件写入出现问题。");
+    }
+    else
+    {
+        Logger::logger.info("[INISystem]文件写入成功。");
+    }
+}
 // 插件实例
 class Main : public CPPPlugin {
 public:
@@ -36,6 +81,8 @@ public:
   void onEnable() override {
     // 请在此处监听
       Logger::logger.info("[Info]插件正在启动。如群内无法接收到机器人消息，则可能处于风控状态。");
+      Logger::logger.info("[System]初始化黑名单数据文件中");
+      iniInitialize(iniPath); //初始化数据
       Event::registerEvent<GroupMessageEvent>([](GroupMessageEvent GroupMessage) { //群内消息的事件注册
           Group ReceivedGroup(GroupMessage.group.id(), GroupMessage.bot.id); // 实例化接收到的群对象
           Group Baozipu(604890935,GroupMessage.bot.id);
@@ -81,9 +128,6 @@ public:
                   int InvalidNickUserCount=0;
                   Logger::logger.info("[Operation]管理员操作:检测群名片。");
                   auto now = std::chrono::system_clock::now();
-                  //通过不同精度获取相差的毫秒数
-                  uint64_t dis_millseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count()
-                                             - std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count() * 1000;
                   time_t tt = std::chrono::system_clock::to_time_t(now);
                   auto time_tm = localtime(&tt);
                   char strTime[25] = { 0 };
@@ -143,11 +187,11 @@ public:
                   Logger::logger.info("[System]当前时间是 " + CurrentTime);
                   isCleanCancelled = 0;
                   Sleep(500);
-                  GroupMessage.group.sendMessage("十秒后将执行不合规成员清理操作。\n如需在操作期间执行取消操作，请输入 .cleancancel。");
-                  Sleep(10000);
                   std::ofstream CheckNickLog("C:\\Users\\Server\\Desktop\\CleanNickLog.txt",std::ios_base::out | std::ios_base::trunc);
                   std::string FirstLine = "群成员名片清理操作，请求时间: ";
                   CheckNickLog << FirstLine << strTime << std::endl;//写出操作时间到日志
+                  GroupMessage.group.sendMessage("十秒后将执行不合规成员清理操作。\n如取消操作，请输入 .cleancancel。\n因线程限制，执行期间无法取消。");
+                  Sleep(10000);
                   if(isCleanCancelled == 0)
                   {
                       GroupMessage.group.sendMessage("清理任务已启动。");
@@ -155,6 +199,7 @@ public:
                       Logger::logger.info("[System]群成员清单拉取完毕，长度 " + std::to_string(MemberList.size()));
                       Sleep(300);
                       GroupMessage.group.sendMessage("获取群成员元素完毕，当前群成员数为 " + std::to_string(MemberList.size()));
+                      Sleep(1500);
                       for(int i=0;i<=MemberList.size()-1;i++)
                       {
                           if(isCleanCancelled == 0)
@@ -171,7 +216,7 @@ public:
                                       std::string OutputResult = "检测到不合规的群名片，正在清理。位置：" + std::to_string(i) + " ,成员号: " + std::to_string(DetectedMember.id()) + " ,群名片为: " + DetectedMember.nickOrNameCard();
                                       CheckNickLog << OutputResult << std::endl;
                                       CleanedUser++;
-                                      Sleep(800);
+                                      Sleep(3200);
                                   }
                                   catch (BotException &err){
                                       Logger::logger.error(err.what());
@@ -215,9 +260,24 @@ public:
                   Sleep(1000);
                   GroupMessage.group.sendMessage("清理完毕，清理了 "+ std::to_string(CleanedUser)+" 位群成员。\n结果已发送至上方群文件。");
               }
-
+              if(GroupMessage.message == MessageChain(PlainText(".testMiraiCode")))
+              {
+                  GroupMessage.group.sendMessage("提示：此命令仅供开发用途。\n此条消息的 MiraiCode 格式为:\n" + GroupMessage.message.toMiraiCode());
+              }
+              if(GroupMessage.message.toMiraiCode().substr(0,10) == ".queryban ")
+              {
+                  std::string QueryMember = GroupMessage.message.toMiraiCode().erase(0, 10);
+                  std::string ResultMsg = iniQuery(iniPath,"banlist", QueryMember);
+                  GroupMessage.group.sendMessage("对 "+ QueryMember + " 的封禁查询：\n" + ResultMsg);
+              }
+              if(GroupMessage.message.toMiraiCode().substr(0,12) == ".queryleave ")
+              {
+                  std::string QueryMember = GroupMessage.message.toMiraiCode().erase(0, 12);
+                  std::string ResultMsg = iniQuery(iniPath,"LeaveRecord", QueryMember);
+                  GroupMessage.group.sendMessage("对 "+ QueryMember + " 的离群查询：\n" + ResultMsg);
+              }
           }
-
+          
       });
       Event::registerEvent<MemberJoinRequestEvent>([](MemberJoinRequestEvent JoinRequestEvent){ //成员申请入群的事件注册
           Group Baozipu(604890935,JoinRequestEvent.bot.id);
@@ -225,32 +285,61 @@ public:
           Group Lianhehui(1070074632,JoinRequestEvent.bot.id);
           if(JoinRequestEvent.group == Baozipu)
           {
-              //目前无可靠方法获取入群玩家的附言，暂时不设置转发。
-              std::string MemberID = std::to_string(JoinRequestEvent.requesterId);
-              Sleep(800);
-              Notify.sendMessage("检测到包子铺入群申请，请及时处理!\n申请者:" + MemberID);
+              //入群申请监听。目前无可靠方法获取入群玩家的附言，暂时不设置转发。
+              std::string RequestID = std::to_string(JoinRequestEvent.requesterId);
+              std::string QueryBanResult = iniQuery(iniPath,"banlist", RequestID);
+              std::string QueryLeaveRecord = iniQuery(iniPath,"LeaveRecord",RequestID);
+              if(QueryBanResult == "未找到")
+              {
+                  Sleep(800);
+                  Notify.sendMessage("检测到包子铺入群申请，请及时处理!\n申请者:" + RequestID + "\n离群记录:" + QueryLeaveRecord);
+              }
+              else
+              {
+                  Sleep(800);
+                  JoinRequestEvent.reject("您已被管理员封禁，拒绝加群。");
+              }
           }
       });
       Event::registerEvent<MemberLeaveEvent>([](MemberLeaveEvent LeaveEvent){ //群成员离开事件监听
           Group Baozipu(604890935,LeaveEvent.bot.id);
           Group Notify(181327275,LeaveEvent.bot.id);
           Group Lianhehui(1070074632,LeaveEvent.bot.id);
+          auto now = std::chrono::system_clock::now();
+          //通过不同精度获取相差的毫秒数
+          time_t tt = std::chrono::system_clock::to_time_t(now);
+          auto time_tm = localtime(&tt);
+          char strTime[25] = { 0 };
+          sprintf(strTime, "%d-%02d-%02d %02d:%02d:%02d", time_tm->tm_year + 1900,
+                  time_tm->tm_mon + 1, time_tm->tm_mday, time_tm->tm_hour,
+                  time_tm->tm_min, time_tm->tm_sec);
+          std::string CurrentTime(strTime);
           if(LeaveEvent.group.id() == 604890935 && isKickIgnoreEnable == 0)
           {
               std::string MemberID = std::to_string(LeaveEvent.memberid);
               std::string LeaveText;
+              std::string LeaveType;
               if(LeaveEvent.type == 1) // Type= 1 - 被踢；2 - 自己退群。
               {
                   LeaveText = " 被送了飞机票。";
+                  LeaveType = "(被踢出)";
               }
               else
               {
                   LeaveText = " 离开了包子铺。";
-
+                  LeaveType = "(主动离开)";
               }
               LeaveEvent.group.sendMessage(MemberID + LeaveText);
-              Sleep(1500);
-              Notify.sendMessage("检测到包子铺离开事件：\n" + MemberID + LeaveText);
+              std::string ResponseTmp = iniQuery(iniPath,"LeaveRecord",MemberID);
+              if(ResponseTmp == "未找到")
+              {
+                  iniWrite(iniPath,"LeaveRecord", MemberID, CurrentTime + LeaveType);
+              }
+              else
+              {
+                  iniWrite(iniPath,"LeaveRecord",MemberID,ResponseTmp + "," + CurrentTime + LeaveType);
+              }
+
           }
       });
       Event::registerEvent<MemberJoinEvent>([](MemberJoinEvent JoinedEvent){
